f8a469fafcfc155f366246ec8d7315069b8c225d
Richard <rkj128@gmail.com>
2020-09-01 19:49:41 -0700

    reset code for local deployment

--- logic.js
+++ logic.js
@@ -1,37 +1,16 @@
-// let fact = document.querySelector('#fact');
-// let factText = document.querySelector('#factText');
-
-// const numberInput = document.querySelector("#numberInput");
-// numberInput.addEventListener('input', getFactFetch);
-
-
-
-// function getFactFetch (){
-//     let number = numberInput.value;
-
-//     fetch('http://numbersapi.com/' +number)
-//         .then(response => response.text())
-//         .then(data => {
-//             if(number != ''){
-//                 fact.style.display = 'block';
-//                 factText.innerText = data;
-//             }
-//             ;
-//         })
-//         .catch(err => console.log(err))
-
-// }       
-
 let fact = document.querySelector('#fact');
 let factText = document.querySelector('#factText');
-var ready = (callback) => {
-if (document.readyState != "loading") callback();
-else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-let number = numberInput.value.toString() || '123'
-    fetch('http://numbersapi.com/'+ number +'/trivia')
-   .then(response => response.text())
+
+const numberInput = document.querySelector("#numberInput");
+numberInput.addEventListener('input', getFactFetch);
+
+
+
+function getFactFetch (){
+    let number = numberInput.value;
+
+    fetch('http://numbersapi.com/' +number)
+        .then(response => response.text())
         .then(data => {
             if(number != ''){
                 fact.style.display = 'block';
@@ -39,5 +18,26 @@ let number = numberInput.value.toString() || '123'
             }
             ;
         })
-        .catch(err => console.log(err)) 
-});
+        .catch(err => console.log(err))
+
+}       
+
+// let fact = document.querySelector('#fact');
+// let factText = document.querySelector('#factText');
+// var ready = (callback) => {
+// if (document.readyState != "loading") callback();
+// else document.addEventListener("DOMContentLoaded", callback);
+// }
+// ready(() => {
+// let number = numberInput.value.toString() || '123'
+//     fetch('http://numbersapi.com/redir.php?URL=http://numbersapi.com'+ number +'/trivia')
+//    .then(response => response.text())
+//         .then(data => {
+//             if(number != ''){
+//                 fact.style.display = 'block';
+//                 factText.innerText = data;
+//             }
+//             ;
+//         })
+//         .catch(err => console.log(err)) 
+// });
